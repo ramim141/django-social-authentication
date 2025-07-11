7a7abf9c63fdca56b4ba416563a3968e4229c50c
ramim141 <ahramu584@.com>
2025-07-12 01:10:40 +0600

    Debug false

--- social_auth_project/settings.py
+++ social_auth_project/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG', 'False') == 'True'
+DEBUG = False
 
 ALLOWED_HOSTS = ['https://django-social-authentication.onrender.com', '.onrender.com']
 
